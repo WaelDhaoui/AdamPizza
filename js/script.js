3106c64fef62224a498a729648d0c924b84c682c
Wael Dhaoui <71949263+WaelDhaoui@users.noreply.github.com>
2023-03-29 18:58:30 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ $('.owl-carousel1').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    slideBy: 2,
     responsive:{
         0:{
-            items:1
+            items:2,
+            margin:7,
         },
         600:{
-            items:2
+            items:2,
+            margin:7,
         },
         1000:{
             items:4
@@ -36,3 +39,48 @@ owl.owlCarousel({
         }
     }
 });
+
+// Start Menu
+let toggle = document.querySelector(".btn_container .toggle")
+let large = document.querySelector(".pizza_content .large");
+let mini = document.querySelector(".pizza_content .mini");
+function animateToggle() {
+    toggle.classList.toggle("active"); 
+    large.style.transition = ".5s";
+    mini.style.transition = ".5s";
+    if(toggle.classList.contains("active")) {
+        large.style.top = "360px";
+        mini.style.top = "-330px";
+        toggle.parentElement.lastElementChild.textContent = "MINI"
+    } else {
+        large.style.top = "0px";
+        mini.style.top = "0px";
+        toggle.parentElement.lastElementChild.textContent = "LARGE"
+    }
+}
+// End Menu
+
+//Start Menu
+let items = document.querySelectorAll(".content .item_content")
+let btn_items = document.querySelectorAll("#d-wrapper .filter li")
+btn_items.forEach(function(ele,i) {
+    ele.onclick = function() {
+        btn_items.forEach(function(e) {
+            e.classList.remove("active")
+        });
+        ele.classList.add("active");
+        if(i == 0) {
+            items[0].parentElement.style.height = "397px";
+        } else {
+            items[0].parentElement.style.height = "300px";
+        }
+        items.forEach(function(e,j) {
+            i == j ? e.classList.add("active") : e.classList.remove("active");
+        });
+    }
+})
+//End Menu
+
+// edit media mini
+let divMini = document.querySelectorAll(".pizza_content .mini .card");
+console.log(divMini);
